ee18131e1c46f430df5d8dc8fdf6847beb225bf2
Ispira <Ispira@users.noreply.github.com>
2016-06-19 03:29:30 -0500

    Imgur extension rewrite

--- Ispyra/extensions/imgur.py
+++ Ispyra/extensions/imgur.py
@@ -1,10 +1,9 @@
-#Imgur extension v1.0.1
+#Imgur extension v1.1.0
 import random
 import asyncio
-
 from discord.ext import commands
 from imgurpython import ImgurClient
-from checks import *
+from checks import prefix
 
 imgur_config = open("./extensions/imgur/config.txt").read().splitlines()
 
@@ -14,36 +13,59 @@ class Imgur():
     def __init__(self, bot):
         self.bot = bot
     
-    @commands.command()
+    ## Imgur search
+    @commands.group(pass_context=True)
     @prefix('|')
-    async def imgur(self, arg: str, *, query: str = ""):
-        """Grab an image from imgur."""
-        if arg.lower() == "reddit":
-            result = self.bot.loop.run_in_executor(None,
-            imgur.subreddit_gallery, query)
-        elif arg.lower() == "search":
-            result = self.bot.loop.run_in_executor(None,
-            imgur.gallery_search, query)
-        elif arg.lower() == "random":
-            result = self.bot.loop.run_in_executor(None,
-            imgur.gallery_random)
-        elif arg.lower() == "top" or arg.lower() == "hot":
-            sort = "viral"
-            if query.lower() == "new":
-                sort = query
-            result = self.bot.loop.run_in_executor(None,
-            imgur.gallery, arg.lower(), sort)
-        else:
-            await self.bot.say(
-                "Usage: `|imgur <reddit <subreddit> | search <query> | random | top [new] | hot [new]>`")
-            return
-        
+    async def imgur(self, ctx):
+        """Search on Imgur!"""
+        #Straight from the 404
+        if ctx.invoked_subcommand is None:
+            await self.bot.say("Zoinks! You've taken a wrong turn. `|help Imgur`")
+    
+    async def post_image(self, itype, query=None):
+        case = {
+            "reddit": lambda: imgur.subreddit_gallery(query),
+            "search": lambda: imgur.gallery_search(query),
+            "random": lambda: imgur.gallery_random(),
+            "top"   : lambda: imgur.gallery("top"),
+            "hot"   : lambda: imgur.gallery("hot"),
+        }
+        function = case.get(itype, None)
+        image = self.bot.loop.run_in_executor(None, function)
         while True:
             await asyncio.sleep(0.25)
-            if result.done():
+            if image.done():
+                image = image.result()
                 break
-        choice = random.choice(result.result())
-        await self.bot.say(choice.link)
+        await self.bot.say(random.choice(image).link)
+
+    #Subreddits
+    @imgur.command()
+    async def reddit(self, sub: str):
+        """Get an image from the specified subreddit."""
+        await self.post_image("reddit", sub)
+    
+    #Search
+    @imgur.command()
+    async def search(self, query: str):
+        """Search imgur for almost anything."""
+        await self.post_image("search", query)
+    
+    #Random lol xD :3
+    @imgur.command()
+    async def random(self):
+        """One free random image."""
+        await self.post_image("random")
+    
+    #Like a virus!
+    @imgur.command()
+    async def viral(self, section: str = "top"):
+        """Get one of the most viral images of the day.
+        
+        Section may be either 'top' or 'hot'."""
+        section = section.lower()
+        if section != "top": section = "hot"
+        await self.post_image(section)
     
 def setup(bot):
     bot.add_cog(Imgur(bot))
