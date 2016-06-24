0564d54f782498033e8ca86f820c660a168205cf
Ispira <Ispira@users.noreply.github.com>
2016-06-24 14:39:27 -0500

    Message pinning and unpinning

--- Ispyra/extensions/messages.py
+++ Ispyra/extensions/messages.py
@@ -25,6 +25,31 @@ class Messages():
         header = "{0} ({1} -> {2}):\n".format(message.author.mention, message.channel.mention, dest.mention)
         await self.bot.send_message(dest, header + message.content)
         await self.bot.delete_message(message)
+    
+    @commands.group(pass_context=True)
+    @prefix('$')
+    @permission(manage_messages=True)
+    async def pin(self, ctx):
+        """Pin or unpin a message."""
+        if ctx.invoked_subcommand is None:
+            await self.bot.say("Usage: `$pin <add | remove> <message ID>`")
+    
+    #Do the (un)pinning
+    async def pin_message(self, mid, chan, pin=True):
+        message = await self.bot.get_message(chan, mid)
+        if pin:
+            await self.bot.pin_message(message)
+        else:
+            await self.bot.unpin_message(message)
+    
+    @pin.command(pass_context=True)
+    async def add(self, ctx, message: str):
+        await self.pin_message(message, ctx.message.channel, True)
+    
+    @pin.command(pass_context=True)
+    async def remove(self, ctx, message: str):
+        await self.pin_message(message, ctx.message.channel, False)
+
 
 def setup(bot):
     bot.add_cog(Messages(bot))
