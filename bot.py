23544352522f72f733c16bd0d1f0fedf79e18472
Ispira <Ispira@users.noreply.github.com>
2016-07-09 00:53:11 -0500

    First functioning version

--- bot.py
+++ bot.py
@@ -1,29 +1,37 @@
 import sys
 import os
+import asyncio
 import logging
 import json
 import discord
 
+from io import StringIO
 from datetime import datetime
 from discord.ext import commands as c
 
 # Assorted things
 date = datetime.now()
 
-# Get the config
+# Get the configs
 with open("config/config.json") as js:
     config = json.load(js)
+with open("config/blacklist.json") as bl:
+    blacklist = json.load(bl)
 
 # Set up the config values
-token      = config["token"]
-bot_name   = config["bot_name"]
-bot_avatar = config["bot_avatar"]
-prefix     = config["command_prefix"]
-version    = config["version"]
-log_file   = config["log_file"]
+owner        = config["owner"]
+token        = config["token"]
+bot_name     = config["bot_name"]
+bot_avatar   = config["bot_avatar"]
+prefix       = config["command_prefix"]
+log_file     = config["log_file"]
+log_messages = config["log_messages"]
+log_commands = config["log_commands"]
+version      = config["version"]
 
 # Set up logging
-log_file = "logs/{0.year}-{0.month}-{0.day}-{0.hour}-{0.minute}_{1}".format(date, log_file)
+timestamp = "{0.year}-{0.month}-{0.day}_{0.hour}-{0.minute}-{0.second}".format(date)
+log_file = f"logs/{timestamp}_{log_file}"
 if not os.path.exists("logs"):
     os.makedirs("logs")
 
@@ -36,7 +44,8 @@ log.addHandler(logging.StreamHandler(sys.stdout))
 description="""
 General purpose chat and administration bot.
 """
-bot = c.Bot(command_prefix=prefix, pm_help=True, description=description)
+bot = c.Bot(c.when_mentioned_or(prefix), pm_help=True, description=description)
+plugins = []
 
 # Events
 @bot.event
@@ -51,19 +60,143 @@ async def on_ready():
                 log.warning("Invalid image file for bot_avatar.")
             except discord.HTTPException as error:
                 log.warning(f"Unabled to update bot profile: {error}")
-    
-    # Log bot status
+
+    # Status header
     log.info("------------------------STATUS------------------------")
     log.info(f"{date}")
     log.info(f"Ispyra v{version}")
-    log.info(f"Logged in as {bot.user.name}")
-    log.info("Botmasters: []")
-    log.info("Plugins: []")
-    log.info("Connected to: []")
+    log.info(f"Logged in as {bot.user.name}#{bot.user.discriminator} ({bot.user.id})")
     log.info("------------------------STATUS------------------------")
 
-@bot.command()
-async def quit():
+@bot.event
+async def on_message(msg):
+    # Log it
+    if log_messages:
+        log.info(f"[{msg.server} - #{msg.channel}] <{msg.author}>: {msg.content}")
+    # Handle the commands
+    await bot.process_commands(msg)
+
+@bot.event
+async def on_command(cmd, ctx):
+    # Log it home skittle
+    if log_commands:
+        command = f"{ctx.message.content}"
+        user = f"{ctx.message.author}#{ctx.message.author.discriminator}"
+        location = f"[{ctx.message.server}] - #{ctx.message.channel}"
+        log.info(f'[COMMAND] `{command}` by `{user}` in `{location}`')
+
+@bot.event
+async def on_command_error(err, ctx):
+    channel = ctx.message.channel
+    if isinstance(err, c.NoPrivateMessage):
+        await bot.send_message(channel, "This command is not available in DMs.")
+    elif isinstance(err, c.CheckFailure):
+        await bot.send_message(channel, "I'm sorry, I'm afraid I can't do that.")
+    elif isinstance(err, c.MissingRequiredArgument):
+        await bot.send_message(channel, "Incorrect argument(s).")
+    elif isinstance(err, c.DisabledCommand):
+        pass
+
+@bot.event
+async def on_server_join(srv):
+    log.info(f"[JOIN] {srv.name}")
+
+@bot.event
+async def on_server_remove(srv):
+    log.info(f"[LEAVE] {srv.name}")
+
+# Global check for all commands
+@bot.check
+def allowed(ctx):
+    return ctx.message.author.id not in blacklist["users"]
+# Built-in check for the bot's owner
+def is_owner():
+    return c.check(lambda ctx: ctx.message.author.id == owner)
+
+# Built-in commands
+# Step the bot
+@bot.command(name="quit")
+@is_owner()
+async def quit_bot():
+    """Shut the bot down."""
+    await bot.say("Shutting down.")
     await bot.logout()
+
+@bot.group(pass_context=True)
+async def plugin(ctx):
+    """Plugin functions."""
+    if ctx.invoked_subcommand is None:
+        await bot.say(" | ".join(plugins))
+
+@plugin.command(name="load")
+@is_owner()
+async def plugin_load(name: str):
+    """Load a plugin."""
+    if name in plugins:
+        await bot.say(f"Plugin {name} is already loaded.")
+        return
+    
+    if not os.path.isfile(f"plugins/{name}.py"):
+        await bot.say(f"No plugin {name} exists.")
+        return
+    
+    try:
+        bot.load_extension(f"plugins.{name}")
+        plugins.append(name)
+        await bot.say(f"Plugin {name} loaded.")
+    except:
+        await bot.say(f"Error loading {name}.")
+
+@plugin.command(name="unload")
+@is_owner()
+async def plugin_unload(name: str):
+    """Unload a plugin."""
+    if name not in plugins:
+        await bot.say(f"Plugin {name} is not loaded.")
+        return
+    
+    try:
+        bot.unload_extension(f"plugins.{name}")
+        plugins.remove(name)
+        await bot.say(f"Plugin {name} unloaded.")
+    except:
+        await bot.say(f"Error unloading {name}.")
+
+@bot.command(name="eval", hidden=True, pass_context=True, enabled=False)
+@is_owner()
+async def evaluate(ctx, *, code: str):
+    """Extremely unsafe eval command."""
+    python = "```py\n{0}\n```"
+    code = code.strip("` ")
+    result = None
+    try:
+        result = eval(code)
+        if asyncio.iscoroutine(result):
+            result = await result
+    except Exception as err:
+        await bot.say(python.format(type(err).__name__ + ": " + str(error)))
+        return
+    
+    await bot.say(python.format(result))
+
+@bot.command(name="exec", hidden=True, pass_context=True, enabled=False)
+@is_owner()
+async def execute(ctx, *, code: str):
+    """If you thought eval was dangerous, wait'll you see exec!"""
+    code = code.strip("```").lstrip("py")
+    result = None
+    env = {}
+    env.update(locals())
+    stdout = sys.stdout
+    redirect = sys.stdout = StringIO()
+    
+    try:
+        exec(code, globals(), env)
+    except Exception as err:
+        await bot.say(python.format(type(err).__name__ + ": " + str(error)))
+    finally:
+        sys.stdout = stdout
+    
+    await bot.say(f"```\n{redirect.getvalue()}\n```")
 
 bot.run(token)
